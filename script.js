6d193a54ef13986fbc87a9294fd14f414300c0d6
Ashvin <ashvinjangid10@gmail.com>
2024-08-21 18:24:47 +0530

    format code and minor improvements

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+async function getBgImage(weatherCondition) {
+	const response = await fetch(
+		`https://api.unsplash.com/search/photos?client_id=Z1yKHI4EcigNc15ysjM6RRwmfMDl3kDyEXtp_T-zEBg&page=1&per_page=1&orientation=landscape&query=${weatherCondition}-weather`,
+		{ method: "GET", mode: "cors" }
+	);
+	const imageData = await response.json();
+
+	const bodyElem = document.querySelector("body");
+	const imageUrl = imageData.results[0].urls.full;
+	bodyElem.style.backgroundImage = `url(${imageUrl})`;
+}
+
 async function getWeatherData(placeName) {
 	const response = await fetch(
 		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${placeName}?unitGroup=metric&key=JPPTP2Z7DMHG2KAFAX5B7745S&contentType=json`,
@@ -12,29 +24,20 @@ async function getWeatherData(placeName) {
 	temp.textContent = weatherData.currentConditions.temp;
 	weatherCondition.textContent = weatherData.currentConditions.conditions;
 
-	getBgImage(weatherData.currentConditions.conditions);
+	getBgImage(weatherCondition.textContent);
 }
 
-async function getBgImage(weatherCondition) {
-	const response = await fetch(
-		`https://api.unsplash.com/search/photos?client_id=Z1yKHI4EcigNc15ysjM6RRwmfMDl3kDyEXtp_T-zEBg&page=1&per_page=1&orientation=landscape&query=${weatherCondition}-weather`,
-		{ method: "GET", mode: "cors" }
-	);
-	const imageData = await response.json();
-
-	const bodyElem = document.querySelector("body");
-	const imageUrl = imageData.results[0].urls.raw;
-	bodyElem.style.backgroundImage = `url(${imageUrl})`;
-}
-
-let placeName = "Jaipur";
-getWeatherData(placeName);
-
-const weatherForm = document.querySelector("form");
-weatherForm.addEventListener("click", (e) => {
+function searchWeather(e) {
 	e.preventDefault();
 	const input = document.querySelector("input");
-	placeName = input.value;
+	const placeName = input.value;
+	input.value = "";
 
 	getWeatherData(placeName);
-});
+}
+
+const weatherForm = document.querySelector("form");
+weatherForm.addEventListener("click", searchWeather);
+
+// Show weather for Jaipur
+getWeatherData("Jaipur");
