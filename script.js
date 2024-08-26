e1cb2199b5687b45f30175de61b85caebe69c0c2
Ashvin <ashvinjangid10@gmail.com>
2024-08-26 16:50:15 +0530

    refactor code

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-async function getBgImage(weatherCondition) {
-	const response = await fetch(
-		`https://api.unsplash.com/search/photos?client_id=Z1yKHI4EcigNc15ysjM6RRwmfMDl3kDyEXtp_T-zEBg&page=1&per_page=1&orientation=landscape&query=${weatherCondition}-weather`,
-		{ method: "GET", mode: "cors" }
-	);
+async function setBgImgByWeatherCondition(weatherCondition) {
+	const unsplashUrl = `https://api.unsplash.com/search/photos?client_id=Z1yKHI4EcigNc15ysjM6RRwmfMDl3kDyEXtp_T-zEBg&page=1&per_page=1&orientation=landscape&query=${weatherCondition}-weather`;
+	const response = await fetch(unsplashUrl, { method: "GET", mode: "cors" });
 	const imageData = await response.json();
 
 	const bodyElem = document.querySelector("body");
@@ -18,37 +16,40 @@ async function getBgImage(weatherCondition) {
 	});
 }
 
-async function getWeatherData(placeName) {
-	const loader = document.querySelector(".loader-wrapper");
-	loader.classList.add("display");
+async function fetchWeatherData(placeName) {
+	const loaderElement = document.querySelector(".loader-wrapper");
+	loaderElement.classList.add("display");
 
-	const response = await fetch(
-		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${placeName}?unitGroup=metric&key=JPPTP2Z7DMHG2KAFAX5B7745S&contentType=json`,
-		{ method: "GET", mode: "cors" }
-	);
+	const visualCrossingUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${placeName}?unitGroup=metric&key=JPPTP2Z7DMHG2KAFAX5B7745S&contentType=json`;
+	const response = await fetch(visualCrossingUrl, {
+		method: "GET",
+		mode: "cors",
+	});
+
 	const weatherData = await response.json();
 
-	const place = document.querySelector("#placeName");
-	const temp = document.querySelector("#temp");
-	const weatherCondition = document.querySelector("#weatherCondition");
-	place.textContent = weatherData.address;
-	temp.textContent = weatherData.currentConditions.temp;
-	weatherCondition.textContent = weatherData.currentConditions.conditions;
+	const placeElem = document.querySelector("#placeName");
+	const tempElem = document.querySelector("#temp");
+	const weatherConditionElem = document.querySelector("#weatherCondition");
 
-	getBgImage(weatherCondition.textContent);
+	placeElem.textContent = weatherData.address;
+	tempElem.textContent = weatherData.currentConditions.temp;
+	weatherConditionElem.textContent = weatherData.currentConditions.conditions;
+
+	setBgImgByWeatherCondition(weatherConditionElem.textContent);
 }
 
 function searchWeather(e) {
 	e.preventDefault();
-	const input = document.querySelector("input");
-	const placeName = input.value;
-	input.value = "";
+	const inputElem = document.querySelector("input");
+	const placeName = inputElem.value;
+	inputElem.value = "";
 
-	getWeatherData(placeName);
+	fetchWeatherData(placeName);
 }
 
 const weatherForm = document.querySelector("form");
 weatherForm.addEventListener("submit", searchWeather);
 
 // Show weather for Jaipur
-getWeatherData("Jaipur");
+fetchWeatherData("Jaipur");
